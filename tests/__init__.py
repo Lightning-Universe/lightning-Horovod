ee72939b454dabb9ddd0d265bb6e8125db3d8839
Jirka <jirka.borovec@seznam.cz>
2023-02-03 05:59:19 +0100

    adding tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,6 @@
-"""Cofigure local testing."""
+"""Configure local testing."""
+
+from pl_horovod import _HOROVOD_AVAILABLE
+
+if not _HOROVOD_AVAILABLE:
+    raise ModuleNotFoundError("Horovod is not installed!")
