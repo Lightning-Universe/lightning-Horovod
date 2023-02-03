4702ce925ffa5637e251b1ffd6208cc320c7c9d0
Jirka <jirka.borovec@seznam.cz>
2023-02-03 16:00:02 +0100

    docs

--- src/pl_horovod/__about__.py
+++ src/pl_horovod/__about__.py
@@ -5,30 +5,12 @@ __license__ = "Apache-2.0"
 __copyright__ = f"Copyright (c) 2021-2023, {__author__}."
 __homepage__ = "https://github.com/Lightning-Devel/PL-Horovod"
 __docs__ = "PyTorch Lightning Strategy for Horovod."
-# todo: consider loading Readme here...
-__long_doc__ = """
-Lightning Horovod
------------------
-
-Horovod allows the same training script to be used for single-GPU, multi-GPU, and multi-node training.
-
-Like Distributed Data Parallel, every process in Horovod operates on a single GPU with a fixed subset of the data.
- Gradients are averaged across all GPUs in parallel during the backward pass,
- then synchronously applied before beginning the next step.
-
-The number of worker processes is configured by a driver application (`horovodrun` or `mpirun`).
- In the training script, Horovod will detect the number of workers from the environment,
- and automatically scale the learning rate to compensate for the increased total batch size.
-
-See the official Horovod documentation for details on installation and performance tuning.
-"""
 
 __all__ = [
     "__author__",
     "__author_email__",
     "__copyright__",
     "__docs__",
-    "__long_doc__",
     "__homepage__",
     "__license__",
     "__version__",
