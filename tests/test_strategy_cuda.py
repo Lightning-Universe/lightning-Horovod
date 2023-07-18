5a9f913c596c674fbff8f8898eee5640bb7041a3
Jirka <jirka.borovec@seznam.cz>
2023-07-18 22:48:03 +0200

    tests

--- tests/test_strategy_cuda.py
+++ tests/test_strategy_cuda.py
@@ -168,7 +168,7 @@ def test_transfer_batch_to_gpu(tmpdir):
 def test_accuracy_metric_horovod():
     import horovod
 
-    num_batches = 200
+    num_batches = 20
     batch_size = 512
     threshold = 0.5
 
@@ -201,7 +201,7 @@ def test_accuracy_metric_horovod():
                 sk_batch_result = sk_metric(dist_preds, dist_target)
                 assert np.allclose(
                     batch_result.numpy(),
-                    sk_batch_result,
+                    np.ndarray([sk_batch_result]),
                     atol=0.05,  # todo: this shall be almost zero
                 ), f"with results: {batch_result.numpy()}\n SK ref: {sk_batch_result}"
 
